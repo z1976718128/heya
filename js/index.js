2792bac8ce90efa4ebd7ce8dd5b3ce9e69f185f8
z1976718128 <1976718128@qq.com>
2019-11-06 19:32:44 +0800

    11.6

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
-$(function(){
-	$(".navbar-toggle").on("click",function(){
+/*
+ * @Author: your name
+ * @Date: 2019-11-04 11:44:47
+ * @LastEditTime: 2019-11-06 18:36:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Elegance\js\index.js
+ */
+$(function () {
+	$(".navbar-toggle").on("click", function () {
 		$(".carousel").toggle()
-		
+
 	})
+
+	// 回到顶部
+	$(window).scroll(function(){
+		console.log($(window).scrollTop())
+		if ($(window).scrollTop()>500){
+			$(".right").fadeIn();
+		}
+		else
+		{
+			$(".right").fadeOut();
+		}
+	});
+	$(".topt").click(function(){
+		$('body,html').animate({scrollTop:0},1000);
+		return false;
+	});
+	// 回到顶部
 })
